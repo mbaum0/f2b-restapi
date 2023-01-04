bccfaadb27a01da411a4062ec564d4af74af1728
Michael Baumgarten <mbaum0@pm.me>
2023-01-04 09:07:21 +0100

    status parsing to json

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, jsonify
+from dataclasses import dataclass
 from fail2ban.client.csocket import CSocket
 
 app = Flask(__name__)
@@ -6,17 +7,25 @@ app = Flask(__name__)
 SOCKET_FILE = '/var/run/fail2ban/fail2ban.sock'
 
 
-def error(reason):
-    """
-    A helper function to form an error response.
-    :param reason: What happened wrong
-    :return: Dict of the response
-    """
+@dataclass
+class F2BJailStatus:
+    currently_banned: int
+    total_banned: int
+    ips: list
 
-    return {'error': True, 'reason': reason}
-
+@dataclass
+class F2BStatus:
+    num_jails: int
+    jails: list
 
 def run_cmd(cmd):
+    """
+    Runs a supported F2B-Client command. Returns a tuple
+    containing: (did_error, result)
+
+    if did_error is True, result is a string errror message, otherwise
+    result is the F2B-Client output from the executed command
+    """
     try:
         client = CSocket(SOCKET_FILE, timeout=20)
         client.settimeout(20)
@@ -25,11 +34,23 @@ def run_cmd(cmd):
 
         # if an error occured
         if ret[0] != 0:
-            return error('invalid response code')
+            return (True, 'invalid response code')
 
-        return {'error': False, 'data': ret[1]}
+        return (False, ret[1])
     except Exception as e:
-        return error('exception occured: ' + str(e))
+        return (True, 'exception occured: ' + str(e))
+
+
+def parse_jail_status_output(data):
+    curr_banned = data[1][1][0][1]
+    total_banned = data[1][1][1][1]
+    ips = data[1][1][2][1]
+    return F2BJailStatus(curr_banned, total_banned, ips)
+
+def parse_status_output(data):
+    num_jails = data[0][1]
+    jails = str.split(data[1][1], ', ')
+    return F2BStatus(num_jails, jails)
 
 
 @app.route('/')
@@ -37,25 +58,37 @@ def home():
     return {"hello": "world"}
 
 # BASIC
+
+
 @app.route('/start')
 def start():
     return run_cmd(["start"])
+
 
 @app.route('/reload')
 def reload():
     return run_cmd(["reload"])
 
+
 @app.route('/reload/<jail>')
 def reload_jail(jail):
     return run_cmd(["reload", jail])
 
+
 @app.route('/stop')
 def stop():
-    return run_cmd(["stop"])
+    return run_cmd(["stop"])[1]
+
 
 @app.route('/status')
 def status():
-    return run_cmd(["status"])
+    failure, data = run_cmd(["status"])
+    if failure:
+        return jsonify({'failure': True, 'data': data})
+    else:
+        parsed = parse_status_output(data)
+        return jsonify({'failure': False, 'data': parsed})
+
 
 @app.route('/ping')
 def ping():
@@ -67,14 +100,21 @@ def ping():
 def start_jail(jail):
     return run_cmd(["start", jail])
 
+
 @app.route('/stop/<jail>')
 def stop_jail(jail):
     return run_cmd(["stop", jail])
 
+
 @app.route('/status/<jail>')
 def status_jail(jail):
     command = ["status", jail]
-    return run_cmd(command)
+    failure, data = run_cmd(command)
+    if failure:
+        return jsonify({'failure': True, 'data': data})
+    else:
+        parsed = parse_jail_status_output(data)
+        return jsonify({'failure': False, 'data': parsed})
 
 
 if __name__ == '__main__':
